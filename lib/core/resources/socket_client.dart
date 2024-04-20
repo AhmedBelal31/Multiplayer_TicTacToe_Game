fe3dffb46406e0e4d03db2bda9fcb54052055ab5
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-04-20 19:50:05 +0200

    Create Socket Client And Methods , Connect To Server

--- lib/core/resources/socket_client.dart
+++ lib/core/resources/socket_client.dart
@@ -1,21 +1,48 @@
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-class SocketClient {
-  IO.Socket? socket;
+// class SocketClient {
+//   IO.Socket? socket;
+//
+//   static SocketClient? _instance;
+//
+//   SocketClient._internal() {
+//     socket = IO.io('https://192.168.1.2:3000', <String, dynamic>{
+//       'transports': ['websocket'],
+//       'autoConnect': false,
+//     });
+//     socket!.onConnect((_) {
+//       print('Connected to server.');
+//     });
+//     socket!.connect();
+//   }
+//
+//   static SocketClient get instance {
+//     _instance ??= SocketClient._internal();
+//     return _instance!;
+//   }
+//
+//
+// }
+//
 
-  static SocketClient? _instance;
+class SocketService {
+  final IO.Socket socket;
 
-  SocketClient._internal() {
-    socket == IO.io('http://192.168.1.2:3000' , <String , dynamic> 
-    {
-      'transports' :['websocket'] ,
-      'autoConnect' : false ,
-    }); 
-    socket!.connect();
+  // Define constant IP address
+  static const String serverIp = 'http://192.168.1.2:3000';
+
+  SocketService()
+      : socket = IO.io(serverIp, <String, dynamic>{
+    'transports': ['websocket'], // Use WebSocket transport
+    'autoConnect': false, // Disable auto-connect to handle connection state manually
+  });
+
+  void defineSocket() {
+    // Handle connection event
+    socket.onConnect((_) {
+      print('Connected to server  .');
+    });
+
+    socket.connect();
   }
-  static SocketClient get instance 
-  {
-    _instance ??= SocketClient._internal();
-    return _instance! ;
-  }
-} 
+}
--- lib/core/resources/socket_methods.dart
+++ lib/core/resources/socket_methods.dart
@@ -1,14 +1,57 @@
+import 'package:flutter/material.dart';
 import 'package:multiplayer_tic_tac_toe_game/core/resources/socket_client.dart';
+import 'package:multiplayer_tic_tac_toe_game/presentation/screens/game_screen.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-class SocketMethods 
-{
-  final _socketClient = SocketClient.instance.socket! ;
 
-  void createRoom(String nickname)
-  {
-    if(nickname.isNotEmpty)
-    {
-      _socketClient.emit('createRoom' , nickname) ;
+
+
+// class SocketMethods {
+//   // final _socketClient = SocketClient.instance.socket!;
+//
+//  final SocketService _socketClient = SocketService();
+//
+//
+//
+//   // EMITS
+//   void createRoom(String nickname) {
+//     _socketClient.defineSocket();
+//     if (nickname.isNotEmpty) {
+//       _socketClient.socket.emit('createRoom', {
+//         'nickname': nickname,
+//       });
+//     }
+//   }
+//   void createRoomSuccessListener(BuildContext context) {
+//     _socketClient!.socket.on('CreateRoomSuccess', (room) {
+//       print(room);
+//       Navigator.pushNamed(context, GameScreen.routeName);
+//     });
+//     }
+// }
+import 'package:flutter/material.dart';
+import 'package:multiplayer_tic_tac_toe_game/core/resources/socket_client.dart';
+import 'package:multiplayer_tic_tac_toe_game/presentation/screens/game_screen.dart';
+
+class SocketMethods {
+  final SocketService _socketClient = SocketService();
+
+  // Method to create a room
+  void createRoom(String nickname) {
+    _socketClient.defineSocket(); // Initialize socket connection
+    if (nickname.isNotEmpty) {
+      _socketClient.socket.emit('createRoom', {
+        'nickname': nickname,
+      });
     }
+  }
+
+  // Method to listen for room creation success event
+  void createRoomSuccessListener(BuildContext context) {
+    _socketClient.socket.on('CreateRoomSuccess', (room) {
+      print(room);
+      Navigator.pushNamed(context, GameScreen.routeName);
+    });
   }
 }
